d864862e170b398189c0e597738cabb7f38380c8
safwanbin2 <safwanridwan321@gmail.com>
2024-04-13 23:39:47 +0600

    about-2

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Meet from "./Meet";
+import TheEcosystem from "./TheEcosystem";
 
 const About = () => {
   return (
     <div>
       <Meet />
+      <TheEcosystem />
     </div>
   );
 };
---
+++ src/pages/about/TheEcosystem.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+
+const TheEcosystem = () => {
+  return (
+    <div className="w-full py-20">
+      <div className="w-11/12 mx-auto flex flex-col justify-center items-center ">
+        <h2 className="text-5xl pb-20">The Cribby Ecosystem</h2>
+        <div className="grid grid-cols-2 gap-36">
+          <div className="flex justify-center items-center gap-5">
+            <div className="bg-primary p-2 rounded-box">
+              <img src="https://reef.io/images/ecosystem-wallet.png" alt="" />
+            </div>
+            <div className="space-y-4">
+              <h3 className="text-pink text-2xl font-medium">Cribby Card</h3>
+              <p className="font-medium">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis mollitia illum aspernatur esse aut commodi aliquam
+                itaque dignissimos officia alias.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-5">
+            <div className="bg-primary p-2 rounded-box">
+              <img src="https://reef.io/images/ecosystem-wallet.png" alt="" />
+            </div>
+            <div className="space-y-4">
+              <h3 className="text-pink text-2xl font-medium">Cribby Card</h3>
+              <p className="font-medium">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis mollitia illum aspernatur esse aut commodi aliquam
+                itaque dignissimos officia alias.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-5">
+            <div className="bg-primary p-2 rounded-box">
+              <img src="https://reef.io/images/ecosystem-wallet.png" alt="" />
+            </div>
+            <div className="space-y-4">
+              <h3 className="text-pink text-2xl font-medium">Cribby Card</h3>
+              <p className="font-medium">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis mollitia illum aspernatur esse aut commodi aliquam
+                itaque dignissimos officia alias.
+              </p>
+            </div>
+          </div>
+          <div className="flex justify-center items-center gap-5">
+            <div className="bg-primary p-2 rounded-box">
+              <img src="https://reef.io/images/ecosystem-wallet.png" alt="" />
+            </div>
+            <div className="space-y-4">
+              <h3 className="text-pink text-2xl font-medium">Cribby Card</h3>
+              <p className="font-medium">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Veritatis mollitia illum aspernatur esse aut commodi aliquam
+                itaque dignissimos officia alias.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TheEcosystem;
