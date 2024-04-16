05483891c1448d1cb0a1698a2917993a750f686f
safwanbin2 <safwanridwan321@gmail.com>
2024-04-16 23:36:44 +0600

    nav done

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
@@ -7,13 +7,16 @@ const Navbar = () => {
   const navLinks = [
     { target: "/home", label: "Home" },
     { target: "/about", label: "About Us" },
+    { target: "/contact", label: "Contact Us" },
   ];
+
   return (
     <>
       <div className="w-full bg-transparent absolute top-0 left-0">
         <header className="py-8 z-10 w-11/12 mx-auto">
-          <nav className="flex justify-between items-center max-container">
+          <nav className="flex justify-between items-center max-container relative">
             <Link
+              to="/"
               style={{ fontFamily: "Lobster, sans-serif" }}
               className="text-4xl font-bold text-primary"
             >
@@ -33,22 +36,19 @@ const Navbar = () => {
                   </li>
                 ))}
                 <li>
-                  <Link
-                    to={"/contact"}
-                    className="font-montserrat leading-normal text-lg p-btn rounded-full"
-                  >
-                    Contact Us
+                  <Link className="font-montserrat leading-normal text-lg p-btn rounded-full">
+                    Cribby Web App
                   </Link>
                 </li>
               </ul>
             </div>
             <div
-              className="hidden max-lg:block cursor-pointer"
+              className="fixed md:hidden top-8 right-6 cursor-pointer bg-gradient-to-b from-neutral to-primary p-2 hover:from-primary hover:to-neutral rounded-full !z-[999]"
               onClick={() => {
                 setIsMenuOpen(!isMenuOpen);
               }}
             >
-              <RxHamburgerMenu className="text-4xl text-primary" />
+              <RxHamburgerMenu className="text-3xl text-white" />
             </div>
           </nav>
         </header>
@@ -56,12 +56,12 @@ const Navbar = () => {
           <div className="">
             <nav className="!z-[999] fixed top-0 right-0 left-0 bottom-0 lg:bottom-auto ">
               <div
-                className="hidden max-lg:block fixed right-0 top-5  px-8 py-4 cursor-pointer"
+                className="hidden max-lg:block fixed right-0 top-5  mr-6 mt-3 cursor-pointer bg-gradient-to-b from-neutral to-primary p-2 hover:from-primary hover:to-neutral  rounded-full"
                 onClick={() => {
                   setIsMenuOpen(!isMenuOpen);
                 }}
               >
-                <AiOutlineClose className="text-4xl text-primary" />
+                <AiOutlineClose className="text-3xl text-white" />
               </div>
               <ul className=" bg-white lg:hidden flex flex-col items-center justify-center h-full bg-base-100">
                 {navLinks.map((item) => (
@@ -77,11 +77,10 @@ const Navbar = () => {
                 ))}
                 <li className="mt-5">
                   <Link
-                    to={"/contact"}
                     className="font-montserrat leading-normal  text-2xl p-btn !py-4 rounded-full"
                     onClick={() => setIsMenuOpen(false)}
                   >
-                    Contact Us
+                    Cribby Web App
                   </Link>
                 </li>
               </ul>
